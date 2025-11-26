991e14c7c0728b48acd6733d426a328ec75f84de
Leywin23 <dominikmikulski23@gmail.com>
2025-11-26 21:02:10 +0100

    Create WPF project

---
+++ Brute-Force-password-cracker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Brute_Force_password_cracker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
